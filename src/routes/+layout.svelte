20541d740b4cceb47f0c73d14632e5d5d6484034
Lasse Wolpmann <lassewolpmann@outlook.de>
2025-07-15 10:00:12 +0300

    First version: - Added initial text and layout - Added portrait - Sections still need more text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,15 @@
 
 	let { children } = $props();
 </script>
-
-{@render children()}
+<header class="sticky top-0">
+	<nav class="bg-gray-100 opacity-95 p-4 flex flex-row items-start gap-5">
+		<a href="#about-me">Home</a>
+		<a href="#education">Education</a>
+		<a href="#work">Work Experience</a>
+		<a href="#projects">Projects</a>
+		<a href="#contact">Contact</a>
+	</nav>
+</header>
+<main class="bg-gray-50 p-4">
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,149 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+<svelte:head>
+	<title>Lasse Wolpmann</title>
+</svelte:head>
+<article id="about-me" class="prose prose-img:rounded-full prose-img:w-64 prose-img:shadow-2xl flex flex-row items-center justify-between scroll-m-96">
+	<div>
+		<img src="portrait.jpg" alt="Portrait">
+	</div>
+	<div>
+		<h1>Lasse Wolpmann</h1>
+		<h3>Embedded Software Developer</h3>
+	</div>
+</article>
+
+<article id="education" class="prose my-10 text-nowrap scroll-m-14">
+	<h1>Education</h1>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5">
+			<span>August 2025</span>
+			<span>August 2022</span>
+		</div>
+
+		<section id="university">
+			<h2>Turku University of Applied Sciences</h2>
+			<h3>B. Eng. Information and Communications Technology</h3>
+			<p>📍 Turku, Finland</p>
+			<a href="https://urn.fi/URN:NBN:fi:amk-2025060921697" target="_blank">Bachelors Thesis</a>
+		</section>
+	</div>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5 text-nowrap">
+			<span>July 2020</span>
+			<span>August 2017</span>
+		</div>
+
+		<section id="vocational-school">
+			<h2>Berufsbildungszentrum Dithmarschen</h2>
+			<h3>Computer Science Expert</h3>
+			📍 Meldorf, Germany
+		</section>
+	</div>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5 text-nowrap">
+			<span>June 2017</span>
+			<span>July 2009</span>
+		</div>
+
+		<section id="high-school">
+			<h2>Meldorfer Gelehrtenschule</h2>
+			<h3>High School Diploma</h3>
+			📍 Meldorf, Germany
+		</section>
+	</div>
+</article>
+
+<article id="work" class="prose my-10 text-nowrap scroll-m-14">
+	<h1>Work Experience</h1>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5">
+			<span>July 2025</span>
+			<span>April 2025</span>
+		</div>
+
+		<section id="trainee">
+			<h2>Student Assistant - Smart Brewery</h2>
+			<h3>Turun AMK Oy</h3>
+			📍 Turku, Finland
+		</section>
+	</div>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5">
+			<span>August 2023</span>
+			<span>September 2021</span>
+		</div>
+
+		<section id="pub">
+			<h2>Runner and Bartender</h2>
+			<h3>The Old Irish Pub</h3>
+			📍 Turku, Finland
+		</section>
+	</div>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5">
+			<span>June 2021</span>
+			<span>January 2021</span>
+		</div>
+
+		<section id="conscript">
+			<h2>Conscript</h2>
+			<h3>Puolustusvoimat - Finnish Defence Forces</h3>
+			📍 Säkylä, Finland
+		</section>
+	</div>
+
+	<div class="flex flex-row gap-10">
+		<div class="flex flex-col items-start justify-between timeline mt-12 border-l-2 border-solid border-l-gray-300 px-5">
+			<span>June 2021</span>
+			<span>January 2021</span>
+		</div>
+
+		<section id="sys-admin">
+			<h2>Systems Administrator (Apprentice)</h2>
+			<h3>Evers-Druck GmbH</h3>
+			📍 Meldorf, Germany
+		</section>
+	</div>
+</article>
+
+<article id="projects" class="prose my-10 text-nowrap scroll-m-14">
+	<h1>Projects</h1>
+
+	<section id="formula-countdown-com">
+		<h2>formulacountdown.com <a href="https://formulacountdown.com" target="_blank" class="no-underline">🔗</a></h2>
+		<h3>Skills</h3>
+		<ul>
+			<li>SvelteKit</li>
+			<li>TypeScript</li>
+			<li>(S)CSS</li>
+			<li>REST APIs</li>
+			<li>Google AdSense Monetization</li>
+			<li>SEO Optimization</li>
+		</ul>
+		<a href="https://plausible.lassewolpmann.work/share/formulacountdown.com?auth=s8CG_qWFHncWtNLfIrpcb" target="_blank">View Site Analytics</a>
+	</section>
+
+	<section id="formula-countdown-ios">
+		<h2>Formula Countdown (iOS) <a href="https://apps.apple.com/de/app/formula-countdown/id6472580786" target="_blank" class="no-underline">🔗</a></h2>
+		<h3>Skills</h3>
+		<ul>
+			<li>Swift</li>
+			<li>SwiftUI</li>
+			<li>App Store Deployment</li>
+		</ul>
+	</section>
+</article>
+
+<article id="contact" class="prose my-10 text-nowrap scroll-m-14">
+	<h1>Contact</h1>
+	<p><a href="https://www.linkedin.com/in/lasse-wolpmann/" target="_blank">LinkedIn</a></p>
+	<p><a href="https://github.com/lassewolpmann/" target="_blank">GitHub</a></p>
+	<p><a href="mailto:lassewolpmann@outlook.de">E-Mail</a></p>
+</article>
